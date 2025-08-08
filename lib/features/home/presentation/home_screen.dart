3130ec1b1a20d74cbfb891e28ad145cec06c8796
yousef123123123 <101371095+yousef123123123@users.noreply.github.com>
2025-08-08 04:45:12 +0300

    animated refresh

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -212,32 +212,3 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
-class AnimatedRefreshWidget extends StatelessWidget {
-  final Future<void> Function() onRefresh;
-  final List<Widget> children;
-
-  const AnimatedRefreshWidget({
-    super.key,
-    required this.onRefresh,
-    required this.children,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return RefreshIndicator(
-      onRefresh: onRefresh,
-      displacement: 80,
-      edgeOffset: 40,
-      color: Colors.deepPurple,
-      backgroundColor: Colors.white,
-      strokeWidth: 3.0,
-      child: CustomScrollView(
-        physics: const BouncingScrollPhysics(
-          parent: AlwaysScrollableScrollPhysics(),
-        ),
-        slivers: [SliverList(delegate: SliverChildListDelegate(children))],
-      ),
-    );
-  }
-}
